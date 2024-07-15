e793c73498960cc5d77d42d6d33ba28cf2e38013
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-15 12:44:05 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -16,22 +16,25 @@ bool StringCalculator::isStringEmpty(std::string input)
   }
 }
 
-int StringCalculator::isValidNumber(const char* token)
+int isValidNumber(const std::string& token) 
 {
-  int num = atoi(token);
-  return (num <= 1000);
+    int num = std::stoi(token);
+    return (num <= 1000);
 }
 
-int StringCalculator::calculateSum(std::string input)
+int calculateSum(const std::string& input) 
 {
     int sum = 0;
-    char* token = strtok((char*)input, ",\n;");
-    while (token != NULL) {
-        if (isValidNumber(token)) {
-            sum += atoi(token);
-        }
-        token = strtok(NULL, ",\n;");
+    std::string delimiter = ",\n;";
+    size_t pos = 0;
+    std::string token;
+    while ((pos = input.find_first_of(delimiter)) != std::string::npos) 
+    {
+        token = input.substr(0, pos);
+        sum += std::stoi(token);
+        input.erase(0, pos + 1);
     }
+    sum += std::stoi(input);
     return sum;
 }
 
