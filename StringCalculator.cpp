4e6dad31310a0d3b3d20f2386b90da7f48ce7ade
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-15 12:27:38 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -14,14 +14,30 @@ bool StringCalculator::isStringEmpty(std::string input)
   }
 }
 
+int StringCalculator::isValidNumber(const char* token)
+{
+  int num = atoi(token);
+  return (num <= 1000);
+}
+
+int StringCalculator::calculateSum(const char* input)
+{
+    int sum = 0;
+    char* token = strtok((char*)input, ",\n;");
+    while (token != NULL) {
+        if (isValidNumber(token)) {
+            sum += atoi(token);
+        }
+        token = strtok(NULL, ",\n;");
+    }
+    return sum;
+}
+
 int StringCalculator::add(std::string input)
 {
   if(isStringEmpty(input))
   {
     return 0;
   }
-  else
-  {
-    return 1;
-  }
+  return calculateSum(input);
 }
