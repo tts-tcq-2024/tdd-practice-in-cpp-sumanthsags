e75af7de125df195cb20ef53f7de8070e8f2b840
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-15 13:03:34 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -28,11 +28,12 @@ int StringCalculator::calculateSum(const std::string& input)
     std::string delimiter = ",\n;";
     size_t pos = 0;
     std::string token;
-    while ((pos = input.find_first_of(delimiter)) != std::string::npos)
+    std::string inputCopy = input;
+    while ((pos = inputCopy.find_first_of(delimiter)) != std::string::npos)
     {
-        token = input.substr(0, pos);
+        token = inputCopy.substr(0, pos);
         sum += std::stoi(token);
-        input.erase(0, pos + 1);
+        inputCopy.erase(0, pos + 1);
     }
     sum += std::stoi(input);
     return sum;
