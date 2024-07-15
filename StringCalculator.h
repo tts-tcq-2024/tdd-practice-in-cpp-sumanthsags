aa1f79b59aa5d43dcf9fcc4bcf802bdc666fd50f
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-15 12:45:45 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,6 +4,6 @@ class StringCalculator
    public:
       int add(std::string input);
       bool isStringEmpty(std::string input);
-      int isValidNumber(const char* token);
-      int calculateSum(std::string input);
+      int isValidNumber(const std::string& token);
+      int calculateSum(const std::string& input);
 };
